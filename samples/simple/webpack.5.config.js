3bec7002f1b99880028e7f0ce8b0b7bc9d72314d
Gilles Zunino <gzunino@hotmail.com>
2020-12-23 11:05:04 -0800

    Simple Sample - Add webpack 5 specific configuration

---
+++ samples/simple/webpack.5.config.js
@@ -0,0 +1,62 @@
+﻿const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+
+const isDevelopment = process.env.NODE_ENV !== "production";
+const generateSourceMaps = isDevelopment;
+
+
+module.exports = {
+    mode: isDevelopment ? "development" : "production",
+    devtool: isDevelopment ? "source-map" : "",
+    target: "web",
+
+    // MiniCssExtractPlugin currentl requires the cache to be disabled for HMR to work
+    cache: isDevelopment ? false : undefined,
+
+    entry: {
+        main: [ "./Scripts/Main.ts" ]
+    },
+
+    output: {
+        path: path.join(__dirname, "wwwroot"),
+        filename: `js/[name].min.js`,
+        publicPath: "/"
+    },
+
+    resolve: {
+        extensions: [ ".ts", ".js", ".less", ".css" ],
+        modules: [
+            path.resolve(__dirname, "Scripts"),
+            path.resolve(__dirname, "Styles"),
+            "node_modules"
+        ]
+    },
+
+    module: {
+        rules: [
+            { test: /\.ts$/i, loader: "ts-loader" },
+            {
+                test: /\.css$/i,
+                use: [
+                    { loader: MiniCssExtractPlugin.loader, options: { } },
+                    { loader: "css-loader", options: { sourceMap: generateSourceMaps } }
+                ]
+            },
+            {
+                test: /\.less$/i,
+                use: [
+                    { loader: MiniCssExtractPlugin.loader, options: { } },
+                    { loader: "css-loader", options: { sourceMap: generateSourceMaps } },
+                    { loader: "less-loader", options: { sourceMap: generateSourceMaps } }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "css/[name].min.css",
+            chunkFilename: "[id].min.css"
+        })
+    ]
+};
