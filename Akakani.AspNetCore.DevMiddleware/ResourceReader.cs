53d8dd5dfd9162e1e428152b9b4ed39162c7e962
Gilles Zunino <gzunino@hotmail.com>
2020-12-23 17:44:56 -0800

    Enable ability to read the content of a resource as a string

---
+++ Akakani.AspNetCore.DevMiddleware/ResourceReader.cs
@@ -0,0 +1,24 @@
+﻿namespace Akakani.AspNetCore.DevMiddleware
+{
+    using System;
+    using System.IO;
+    using System.Reflection;
+
+
+    internal static class ResourceReader
+    {
+        public static string Read(Type assemblyContainingType, string path)
+        {
+            Assembly assembly = assemblyContainingType.GetTypeInfo().Assembly;
+            string embeddedResourceName = assembly.GetName().Name + path.Replace("/", ".");
+
+            using (Stream stream = assembly.GetManifestResourceStream(embeddedResourceName))
+            {
+                using (StreamReader streamReader = new StreamReader(stream))
+                {
+                    return streamReader.ReadToEnd();
+                }
+            }
+        }
+    }
+}
