c831b4db67142d63eeda3950735ca6c05b6a4895
Gilles Zunino <gzunino@hotmail.com>
2021-03-14 23:55:51 -0700

    Webpack dev server logger and console.log stream processor

---
+++ Akakani.AspNetCore.DevMiddleware/Webpack-Devserver/ConsoleStreamProcessor.ts
@@ -0,0 +1,31 @@
+import Logger from "./Logger";
+
+export default class ConsoleStreamProcessor {
+
+    private static readonly FindNewlinesRegex: RegExp = /\n(?!$)/g;
+    private static readonly EncodedNewline: string = "__awpdm_nl__";
+
+    public constructor(process: NodeJS.Process) {
+        // Intercept writes to stdErr and stdOut and replace line breaks with a marker token
+        // .NET code converts the token back to Environment.NewLine and logs to ILogger
+        // This preserves multiline log entries as multiline instead of multiple independent log messages
+        this.encodeNewlines(process.stdout, Logger.InformationPostfix);
+        this.encodeNewlines(process.stderr, Logger.ErrorPostfix);
+    }
+
+    private encodeNewlines(outputStream: NodeJS.WritableStream, postfix: string): void {
+        const origWriteFunction = outputStream.write;
+        outputStream.write = <any> function (this: any, value: any) {
+            if (typeof value === "string") {
+                const argsClone = Array.prototype.slice.call(arguments, 0);
+                if (!value.startsWith(Logger.LogPrefix)) {
+                    value = `${Logger.LogPrefix}${postfix} ${value}`;
+                }
+                argsClone[0] = value.replace(ConsoleStreamProcessor.FindNewlinesRegex, ConsoleStreamProcessor.EncodedNewline);
+                origWriteFunction.apply(this, argsClone);
+            } else {
+                origWriteFunction.apply(this, arguments);
+            }
+        };
+    }
+}
---
+++ Akakani.AspNetCore.DevMiddleware/Webpack-Devserver/Logger.ts
@@ -0,0 +1,19 @@
+export default class Logger {
+    public static readonly LogPrefix: string = "[Akakani.AspNetCore.DevMiddleware.WebpackDevServer]";
+
+    public static readonly ErrorPostfix: string = "[ERR]";
+    public static readonly WarningPostfix: string = "[WRN]";
+    public static readonly InformationPostfix: string = "[INF]";
+
+    public error(message: string, ...optionalParams: any[]): void {
+        console.error.apply(this, [ `${Logger.LogPrefix}${Logger.ErrorPostfix} ${message}`, ...optionalParams ]);
+    }
+
+    public warning(message?: any, ...optionalParams: any[]): void {
+        console.warn.apply(this, [ `${Logger.LogPrefix}${Logger.WarningPostfix} ${message}`, ...optionalParams ]);
+    }
+
+    public information(message?: any, ...optionalParams: any[]): void {
+        console.info.apply(this, [ `${Logger.LogPrefix}${Logger.InformationPostfix} ${message}`, ...optionalParams ]);
+    }
+}
